9bb41fdd00088da80cd73ea88e09bf41c018b9b9
pawankrgithub <132369676+pawankrgithub@users.noreply.github.com>
2023-05-15 14:32:03 +0530

    modul4 add

---
+++ mod4/mod4_solution/sript.js
@@ -0,0 +1,20 @@
+var names=new Array();
+names[0]="pawan";
+names[1]="John";
+names[2]="jen";
+names[3]="jason";
+names[4]="abhisek";
+names[5]="ravi";
+names[6]="roshan";
+names[7]="Rahul";
+names[8]="udit";
+names[9]="tarun";
+
+for(var i=0;i<name.length;i++){
+    if(names[i].charAt(0)==='j'|| names[i].charAt(0)==='J'){
+      console.log("Goodbye"+names[i])  
+    }
+    else{
+        console.log("Hello "+nmaes[i])
+    }
+}
